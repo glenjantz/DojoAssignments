0914b86bf140ece8f68cbc28092663a40229ced9
glenjantz <glenjantz@yahoo.com>
2017-01-30 14:42:24 -0800

    added functionality for randomchance.js

---
+++ WebFundamentals/javascript/randomchance/randomchance.js
@@ -0,0 +1,12 @@
+function randomchance(numq){
+  var winnings = 0;
+  for (var i=numq;i>=1;i--){     //countdown how many quarters you have
+    var reward = Math.floor(Math.random() *50)+50;      //calculates a random reward winning between 50 and 100
+    if(Math.floor(Math.random()*100)+1 ===2){          //chance to win and variable to win =2
+      winnings = i + reward;                   // winnings = reward + leftover quarters
+      console.log(winnings);                     // logs how much was rewarded + leftover quarters
+    }
+  }
+  return "0";                                 // returns 0 if you ran out of quarters
+}
+console.log(randomchance(100))
