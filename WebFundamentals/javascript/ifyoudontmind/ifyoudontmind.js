01d72ee54ea2377e8a397d970bf9c29fc3748818
glenjantz <glenjantz@yahoo.com>
2017-01-29 18:06:24 -0800

    added birthday countdown assignments and clock assignment

---
+++ WebFundamentals/javascript/ifyoudontmind/ifyoudontmind.js
@@ -0,0 +1,15 @@
+var HOUR = 8;
+var MINUTE = 5;
+var PERIOD = "AM";
+// var past = HOUR+1;
+
+if(MINUTE > 30)
+console.log("It's almost",HOUR + 1,"in the morning");
+if(MINUTE > 14 && MINUTE < 16)
+console.log("It's quarter after",HOUR);
+if(MINUTE === 5)
+console.log("It's 5 after", HOUR)
+else if(MINUTE < 30)
+console.log("It's just past",HOUR,PERIOD);
+if(MINUTE === 30)
+console.log("It's half past",HOUR);
---
+++ WebFundamentals/javascript/whileyouwait/whileyouwait.js
@@ -0,0 +1,14 @@
+for(var i=60;i>=0;i--){
+  if(i===0){
+    console.log("IT'S MY BIRTHDAY YESSSSSSS!!!!!!!")
+  }
+  if(i<=5 && i!=0){
+    console.log(i+" DAYS UNTIL MY BIRTHDAY!!!");
+  }
+  else if(i<=30 && i!=0){
+    console.log(i+" days until my birthday wooooo!");
+  }
+  if(i>30){
+    console.log(i+" days until my birthday. :( boooo");
+  }
+}
