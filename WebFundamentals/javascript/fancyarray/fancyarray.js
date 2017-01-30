302d2b620cfecb31ce51e572bddc3bfc363746df
glenjantz <glenjantz@yahoo.com>
2017-01-30 12:23:08 -0800

    added variable arr, loop to  loop through the array, log each index of the array

---
+++ WebFundamentals/javascript/fancyarray/fancyarray.js
@@ -0,0 +1,4 @@
+var arr = ["James","Jill","Jane","Jack"];  // declares an array with 4 strings
+for(var i=0;i<arr.length;i++){             //starts a loop where i is the variable and if its lets than the array length runs the code
+  console.log(arr[i]);                     //logs the current index of i
+}
