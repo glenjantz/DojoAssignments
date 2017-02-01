fb11fc5d835679f84e7c3d5d31b96a01e51f28b3
glenjantz <glenjantz@yahoo.com>
2017-02-01 14:58:48 -0800

    fixed debugging-jquery&randomchance, added more jquery functions, added hoverinout function, added shell for users

--- WebFundamentals/javascript/randomchance/randomchance.js
+++ WebFundamentals/javascript/randomchance/randomchance.js
@@ -1,7 +1,7 @@
 function randomchance(numq){
   var winnings = 0;
   for (var i=numq;i>=1;i--){     //countdown how many quarters you have
-    var reward = Math.floor(Math.random() *50)+50;      //calculates a random reward winning between 50 and 100
+    var reward = Math.floor(Math.random() *50+1)+50;      //calculates a random reward winning between 50 and 100
     if(Math.floor(Math.random()*100)+1 ===2){          //chance to win and variable to win =2
       winnings = i + reward;                   // winnings = reward + leftover quarters
       console.log(winnings);                     // logs how much was rewarded + leftover quarters
