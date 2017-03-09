0bede7fffadb2b84eb348c9812fd98cecca2176a
Glen Jantz <glenjantz@Glens-MacBook-Pro.local>
2017-03-09 13:56:07 -0800

    added base for tip calculator

--- iOS/tipster/tipster/ViewController.swift
+++ iOS/tipster/tipster/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var lowTip: UILabel!
+    @IBOutlet weak var midTip: UILabel!
+    @IBOutlet weak var highTip: UILabel!
+    
+    
+    @IBOutlet weak var lowTipNum: UILabel!
+    @IBOutlet weak var midTipNum: UILabel!
+    @IBOutlet weak var highTipNum: UILabel!
+    
+    @IBOutlet weak var lowTipTotal: UILabel!
+    @IBOutlet weak var midTipTotal: UILabel!
+    @IBOutlet weak var highTipTotal: UILabel!
+    
+    
+    @IBOutlet weak var groupSize: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +41,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+    }
+    
+    
 
+    @IBAction func groupSlider(_ sender: UISlider) {
+    }
+
+    
+    @IBAction func tipSlider(_ sender: UISlider) {
+    }
+    
 }
 
