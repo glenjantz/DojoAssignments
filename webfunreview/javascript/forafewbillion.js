1a22199b8716e999c9bec46e81de55131731afaf
Glen Jantz <glenjantz@yahoo.com>
2017-08-09 13:01:23 -0400

    more js review

---
+++ webfunreview/javascript/forafewbillion.js
@@ -0,0 +1,31 @@
+// There is an old tale about a king who offered a servant $10,000 as a reward. The servant instead asked that for 30 days he be given an amount that would double,
+//  starting with a penny. (1 penny for the first day, 2 for the second, 4 for the third, then 8, 16, 32 pennies, and so on).
+// Use for loops to answer the following:
+// How much was the reward after 30 days?
+// remember, a penny isn't 1, but 0.01
+// Bonus (Only If You Have Time):
+// How many days would it take the servant to make $10,000?
+// How about 1 billion?
+// In JavaScript, there is a value Infinity, how many days until the servant has infinite money?
+
+// var start = 0.01;
+// for(var i =1; i<=30; i++){
+//   start = start + start;
+//   console.log("money on this day", i, "is this amount", start)
+// }
+
+function daysToReward(howmuch){
+  var total = 0.01
+  var reward = 0.01;
+  var days = 1;
+  while(total <= howmuch){
+    reward = reward * 2;
+    total = reward + total;
+    days++
+  }
+  return days
+}
+
+console.log("how many days for $10,000", daysToReward(10000))
+console.log("how many days for $1,000,000,000", daysToReward(1000000000))
+console.log("how many days for infinity", daysToReward(Infinity))
