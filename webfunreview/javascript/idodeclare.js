7b60e3e5deb3d8303b641873ca2defced94cb385
Glen Jantz <glenjantz@yahoo.com>
2017-08-09 12:24:59 -0400

    javascript basics review

---
+++ webfunreview/javascript/idodeclare.js
@@ -0,0 +1,29 @@
+// Declare variables of the following types:
+// 4 Numbers
+// 3 Strings
+// 2 Booleans
+// 1 Undefined Variable
+// Give the variables descriptive names (ex: string1 and string2)
+// Print each one to the console with a description (ex: console.log("The First String:", string1);)
+// Try to be creative! (don't use 1, 2, 3, and 4 for your numbers -- test out negatives or decimals!)
+
+var one = 1;
+var two = 2;
+var three = 3;
+var four = 4;
+var string1 = "one";
+var string2 = "two";
+var string3 = "three";
+var yes = true;
+var no = false;
+var maybe;
+console.log("first num", one)
+console.log("second num", two)
+console.log("third num", three)
+console.log("fourth num", four)
+console.log("first string", string1)
+console.log("second string", string2)
+console.log("third string", string3)
+console.log("first bool", yes)
+console.log("second bool", no)
+console.log("undef", maybe)
