adff99722733a9aa85f60387a4f91b62ba5a11a2
Glen Jantz <glenjantz@yahoo.com>
2017-08-09 13:48:14 -0400

    more js review

---
+++ webfunreview/javascript/fancyarray.js
@@ -0,0 +1,26 @@
+// Normally, if you print an array such as ["James", "Jill", "Jane", "Jack"], you will see the following:
+// [ "James", "Jill", "Jane", "Jack" ]
+// Let's make it look fancy! Write a function that will make it print like:
+// 0 -> James
+// 1 -> Jill
+// 2 -> Jane
+// 3 -> Jack
+// Bonus (Only If You Have Time):
+// Let the user pass in the symbol that will print (ex: "->", "=>", "::", "-----")
+// Have an extra parameter reversed. If the user passes true, print the elements in reverse order
+
+function fancyArr(arr, symbol = "->", reversed = false){
+  if(reversed){
+    for(var i=0;i<arr.length/2;i++){
+      var temp = arr[i];
+      arr[i] = arr[arr.length-1-i];
+      arr[arr.length-1-i] = temp;
+    }
+  }
+  for (var i = 0; i < arr.length; i++){
+    console.log(i+" "+symbol+" "+arr[i]);
+  }
+}
+// fancyArr(["james","jill","jane","jack"])
+// fancyArr(["james","jill","jane","jack"], "---")
+fancyArr(["james","jill","jane","jack","bob","billy","jose"], "---", true)
