466a98f279bd7dcb17277d28d6c11b93a8bcb1ce
Glen Jantz <glenjantz@yahoo.com>
2017-12-07 15:31:23 -0500

    inheritance practice

---
+++ Ruby/first_vagrant_box/OOP/dog/dog.rb
@@ -0,0 +1,17 @@
+require_relative 'mammal'
+class Dog < Mammal
+  def pet
+    @health += 5
+    self
+  end
+  def walk
+    @health -= 1
+    self
+  end
+  def run
+    @health -= 10
+    self
+  end
+end
+d = Dog.new
+d.walk.walk.walk.run.run.pet.display_health
---
+++ Ruby/first_vagrant_box/OOP/dog/mammal.rb
@@ -0,0 +1,13 @@
+class Mammal
+  attr_accessor :health
+  def initialize
+    @health = 150
+    self
+  end
+  def display_health
+    puts "the mammals health is #{@health}"
+    self
+  end
+end
+# h = Mammal.new
+# puts h.display_health
