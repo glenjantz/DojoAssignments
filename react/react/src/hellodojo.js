1c59e7741f9f20de5a2e653e43b007381d1c2e68
Glen Jantz <glenjantz@yahoo.com>
2018-07-10 12:31:12 -0400

    jsx

---
+++ react/react/src/hellodojo.js
@@ -0,0 +1,13 @@
+const element = (
+  <div>
+    <h1> Hello Dojo! </h1>
+    <h2> Things I need to do: </h2>
+    <ul>
+      <li>Learn React</li>
+      <li>Climb Mt. Everest!</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
+    </ul>
+  </div>
+);
+ReactDOM.render(element, document.getElementById('root'))
--- react/react/tickingclockreact.js
+++ react/react/tickingclockreact.js
@@ -1,5 +1,21 @@
-
-$(document).ready(function(){
-  const element = <h1>Hello, world</h1>;
+function tick() {
+  var element = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Hello, world!'
+    ),
+    React.createElement(
+      'h2',
+      null,
+      'It is ',
+      new Date().toLocaleTimeString(),
+      '.'
+    )
+  );
   ReactDOM.render(element, document.getElementById('root'));
-})
+}
+
+setInterval(tick, 1000);
